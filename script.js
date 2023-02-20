46a6e158f6e5855001563eaac09e5c0e22e5742b
Sebastian S <sebastiansiewweiqi@gmail.com>
2023-02-20 16:27:27 +0800

    Added 4 basic operations in script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// add
+function add(a, b) {
+  return a + b;
+}
+// subtract
+function subtract(a, b){
+    return a - b;
+}
+// multiply
+function multiply(a, b){
+    return a * b;
+}
+// divide
+function divide(a, b){
+    return a / b;
+}
