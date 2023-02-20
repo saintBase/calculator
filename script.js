8899251223faef49738ac2dce120e0f31e83ebae
Sebastian S <sebastiansiewweiqi@gmail.com>
2023-02-20 16:31:43 +0800

    Added operate function

--- script.js
+++ script.js
@@ -3,14 +3,30 @@ function add(a, b) {
   return a + b;
 }
 // subtract
-function subtract(a, b){
-    return a - b;
+function subtract(a, b) {
+  return a - b;
 }
 // multiply
-function multiply(a, b){
-    return a * b;
+function multiply(a, b) {
+  return a * b;
 }
 // divide
-function divide(a, b){
-    return a / b;
+function divide(a, b) {
+  return a / b;
+}
+
+// operate
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      return "Input not valid.";
+  }
 }
