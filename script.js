a74e715c4e2abb2c85f0d9314c67a47886eefba6
Sebastian S <sebastiansiewweiqi@gmail.com>
2023-02-22 00:50:27 +0800

    Added working calculator functions

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const numberBtn = document.querySelectorAll("[data-number]");
+const operatorBtn = document.querySelectorAll("[data-operator]");
 const screen = document.querySelector(".screen");
+const equalBtn = document.querySelector("[data-equal]");
+const clearBtn = document.querySelector("#clear");
+
+// global variable
+let tempNum1 = 0;
+let tempNum2 = 0;
+let tempOperator = "";
 
 numberBtn.forEach((btn) =>
   btn.addEventListener("click", () => populate(btn.textContent))
 );
+
+operatorBtn.forEach((op) =>
+  op.addEventListener("click", () =>
+    storeNum(screen.textContent, op.textContent)
+  )
+);
+
+equalBtn.addEventListener("click", () => answer(screen.textContent));
+
+clearBtn.addEventListener("click", () => clear());
 
 // add
 function add(a, b) {
@@ -29,9 +47,9 @@ function operate(operator, num1, num2) {
       return add(num1, num2);
     case "-":
       return subtract(num1, num2);
-    case "*":
+    case "x":
       return multiply(num1, num2);
-    case "/":
+    case "÷":
       return divide(num1, num2);
     default:
       return "Input not valid.";
@@ -40,10 +58,30 @@ function operate(operator, num1, num2) {
 
 // populate display
 function populate(input) {
-  console.log(input);
   if (screen.textContent === "0") {
     screen.textContent = input;
   } else {
     screen.textContent += input;
   }
+}
+
+// store number when operator is pressed
+function storeNum(num, op) {
+  tempNum1 = num;
+  tempOperator = op;
+  // TODO:
+  // display should reset/retain number in another div
+  // above main screen
+  screen.textContent = 0;
+}
+
+// call operate when equal is pressed
+function answer(num2) {
+  tempNum2 = num2;
+  screen.textContent = operate(tempOperator, tempNum1, tempNum2);
+}
+
+// clear button function
+function clear() {
+  screen.textContent = 0;
 }
